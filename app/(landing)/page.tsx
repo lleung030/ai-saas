432d5123f4036587599d657108983d90c3a1fb1c
lleung030 <lucasleung2593@gmail.com>
2023-07-25 10:42:45 -0700

    login & register pages

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,19 @@
-import Image from 'next/image'
-import {Button} from '@/components/ui/button'
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function LandingPage() {
   return (
     <div>
-        Landing Page
+      Landing Page (Unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
