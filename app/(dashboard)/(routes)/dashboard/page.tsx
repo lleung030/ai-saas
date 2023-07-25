8478d72729af2da255d45981efed0cfee9eb1d70
lleung030 <lucasleung2593@gmail.com>
2023-07-25 10:45:52 -0700

    user button for signout

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import {Button} from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 export default function DashboardPage() {
   return (
-    <p>Dashboard Page (Protected)</p>
+    <div>
+        <p>Dashboard Page (Protected)</p>
+        <UserButton afterSignOutUrl='/'/>
+    </div>
   )
 }
