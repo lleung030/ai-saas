ab2f0587abe080a55633f4dd993ecaae86896eb5
lleung030 <lucasleung2593@gmail.com>
2023-07-25 17:56:39 -0700

    useRouter in dashboard tools

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -9,6 +9,7 @@ import {
 } from "lucide-react";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import { useRouter } from 'next/navigation';
 
 const tools = [
   {
@@ -49,6 +50,7 @@ const tools = [
 ];
 
 export default function DashboardPage() {
+    const router = useRouter();
   return (
     <div>
       <div className="mb-8 space-y-4">
@@ -65,6 +67,7 @@ export default function DashboardPage() {
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map((tool) => (
           <Card
+            onClick={() => router.push(tool.href)}
             key={tool.href}
             className="p-4 border-black-5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
           >
