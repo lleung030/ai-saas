a59195a1566eff741fd5f8dafa122a093779a0e6
lleung030 <lucasleung2593@gmail.com>
2023-07-25 17:28:22 -0700

    sheet from shadcn-ui

--- components/ui/mobile-sidebar.tsx
+++ components/ui/mobile-sidebar.tsx
@@ -2,13 +2,22 @@
 
 import { Button } from '@/components/ui/button';
 import { Menu } from 'lucide-react'
+import { Sheet, SheetTrigger, SheetContent } from '@/components/ui/sheet';
+import SideBar from './sidebar';
 
 const MobileSidebar = () => {
     return (
+        <Sheet>
+            <SheetTrigger>
+                
         <Button variant='ghost' size='icon' className='md:hidden'>
             <Menu />
-
         </Button>
+            </SheetTrigger>
+            <SheetContent side='left' className='p-0'>
+                 <SideBar />
+            </SheetContent>
+        </Sheet>
     )
 }
 
